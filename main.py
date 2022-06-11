a556400328f3db644777e3955113fa12af5e1df2
Timotimyp <60345296+Timotimyp@users.noreply.github.com>
2022-06-11 23:45:05 +1000

    Update main.py

--- main.py
+++ main.py
@@ -304,4 +304,5 @@ if __name__ == "__main__":
             category.forms_id = '[]'
             db_sess.add(category)
             db_sess.commit()
-    app.run(port=3000, host="127.0.0.1")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
